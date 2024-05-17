e28198ec0e12badbbfb7844f467d8fc655071d59
Neill Engelbrecht <engelbrecht.neill@gmail.com>
2024-05-17 12:54:00 +0200

    removed branch specification to fix error caused by upgrading to nvim 0.10

--- lua/config/plugins/bufferline.lua
+++ lua/config/plugins/bufferline.lua
@@ -1,24 +1,31 @@
 return {
-    "akinsho/bufferline.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons"
+  "akinsho/bufferline.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
+  opts = {
+    options = {
+      mode = "tabs",
+      separator_style = "slant",
+
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          highlight = "Directory",
+          text_align = "left"
+        }
+      },
     },
-    version = "*",
-    opts = {
-      options = {
-        mode = "tabs",
-        separator_style = "slant"
-      },
-      
-      -- Disables italics for the selected buffer
-      highlights = {
-        buffer_selected = { italic = false },
-        diagnostic_selected = { italic = false },
-        hint_selected = { italic = false },
-        pick_selected = { italic = false },
-        pick_visible = { italic = false },
-        pick = { italic = false },
-      },
-    }
+
+    -- Disables italics for the selected buffer
+    highlights = {
+      buffer_selected = { italic = false },
+      diagnostic_selected = { italic = false },
+      hint_selected = { italic = false },
+      pick_selected = { italic = false },
+      pick_visible = { italic = false },
+      pick = { italic = false },
+    },
   }
-  
+}
