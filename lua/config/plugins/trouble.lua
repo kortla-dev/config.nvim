f1abf93a9ec5b740e3dbd734cbefbc732f37a2e6
Neill Engelbrecht <engelbrecht.neill@gmail.com>
2024-05-30 17:16:20 +0200

    same as last commit but i actually did the thing

--- lua/config/plugins/trouble.lua
+++ lua/config/plugins/trouble.lua
@@ -2,15 +2,11 @@ return {
   "folke/trouble.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim",
   },
   keys = {
     { "<leader>xx", "<Cmd>Trouble diagnostics toggle<CR>", desc = "Open/close trouble list" },
-    { "<leader>xw", "<Cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<Cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<Cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<Cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-    { "<leader>xt", "<Cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+    { "<leader>xq", "<Cmd>Trouble qflist toggle<CR>", desc = "Open trouble quickfix list" },
+    { "<leader>xl", "<Cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
   },
   opts = {},
 }
