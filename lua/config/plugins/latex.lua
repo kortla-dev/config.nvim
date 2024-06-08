74d92fd4764696f97fba22d289d7149ec938bf27
Neill Engelbrecht <engelbrecht.neill@gmail.com>
2024-06-08 09:26:45 +0200

    pref changes and the like

--- lua/config/plugins/latex.lua
+++ lua/config/plugins/latex.lua
@@ -3,8 +3,19 @@ return {
   lazy = false,
   init = function()
     -- vim.g.vimtex_view_method = "mupdf"
-    vim.g.vim_compiler_method = "latexrun"
     -- this doesnt want to work so just put the comment at the top of you main.tex
-    vim.g.vimtex_compiler_engine = "xelatex" -- %! TEX TS-program = xelatex
+    -- vim.g.vimtex_compiler_engine = "xelatex" -- %! TEX TS-program = xelatex
+
+    -- vim.g.vim_compiler_method = "latexmk"
+    vim.g.vimtex_compiler_latexmk = {
+      executable = "latexmk",
+      continuous = 0,
+      options = {
+        "-xelatex",
+        "-quiet",
+        "-interaction=nonstopmode",
+        "-synctex=1",
+      },
+    }
   end,
 }
